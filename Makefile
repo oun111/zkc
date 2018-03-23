fd648cdbe41c929dc6bc0e60efb7115a3de450d2
user1 <shadow1212@21cn.com>
2018-03-23 09:19:46 +0800

    minor changes

--- Makefile
+++ Makefile
@@ -1,17 +1,21 @@
 
 
-SRCS        := $(shell find ./ -name "*.c" )
-OBJS        := $(patsubst %.c,%.o,$(SRCS))
+ZKCBASE         := /mnt/sda5/zyw/work/zookeeper-3.4.10/src/c/install
+ZKCINCLUDE      := $(ZKCBASE)/include/zookeeper/
+ZKCLIB          := $(ZKCBASE)/lib/
+
+SRCS            := $(shell find ./ -name "*.c" )
+OBJS            := $(patsubst %.c,%.o,$(SRCS))
 
 #MOD_VER         := $(shell grep -i "__mod_ver__" ./$(MODULE).h|awk '{print $$3}'|sed 's/\"//g')
-LDFLAG          := -Wall -g -pthread -lrt 
-CFLAG_OBJS      := -Wall -Werror -I. -g 
+LDFLAG          := -Wall -g -pthread -lrt  -L$(ZKCLIB) -lzookeeper_mt
+CFLAG_OBJS      := -Wall -Werror -I. -g -I$(ZKCINCLUDE)
 TARGET          := zkc
 
 
 .PHONY: all
 all: $(OBJS)
-	$(CC) $(LDFLAG) $(OBJS) -o $(TARGET)
+	$(CC) $(OBJS) $(LDFLAG) -o $(TARGET)
 
 $(OBJS):%.o:%.c
 	$(CC) $(CFLAG_OBJS) -c $< -o $@
--- zkc.c
+++ zkc.c
@@ -2,125 +2,462 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h> 
-#include <limits.h> 
 #include <string.h> 
 #include <unistd.h> 
+#include <stdbool.h> 
+#include <sys/time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <assert.h>
+#include "zkc.h"
 
 
-typedef struct app_info {
-  /* entry point in zookeeper */
-  char entry[PATH_MAX];
-
-  /* obsolute path of application */
-  char app[PATH_MAX] ;
- 
-  /* obsolute path of application's config */
-  char conf[PATH_MAX] ;
-
-} appInfo ;
-
-appInfo g_appInfo ;
+zkcInfo g_zkcInfo ;
 
 
-void help(void)
+
+int get_master_addr(void)
 {
-  fprintf(stderr,
-"\nStarts a zookeeper cluster. \n"
-"%s <options>\n"
-"options: \n"
-"-c config file\n"
-"-p application file\n"
-"-e zookeeper entry\n",
-  "zkc"
-  );
-  exit(-1);
+  int i = 0;
+  struct ifreq *ifr;
+  struct ifconf ifc;
+  char buf[1024];
+  int fd = socket(AF_INET,SOCK_DGRAM,0);
+
+  if (fd<0) {
+    return -1;
+  }
+
+  /* iterate all local interfaces */
+  ifc.ifc_len = sizeof(buf);
+  ifc.ifc_buf = buf;
+  if (ioctl(fd,SIOCGIFCONF,(char*)&ifc)<0) {
+    /* XXX: error message here */
+    return -1; 
+  }
+
+  for (i=ifc.ifc_len/sizeof(struct ifreq),
+    ifr = ifc.ifc_req; --i>=0; ifr++) 
+  {
+    if (!strcmp(ifr->ifr_ifrn.ifrn_name,"lo")) {
+      continue ;
+    }
+
+    char *ipaddr = inet_ntoa(((struct sockaddr_in*)
+      &ifr->ifr_ifru.ifru_addr)->sin_addr);
+
+    //printf("addr: %s\n",);
+    strcpy(g_zkcInfo.ipAddr,ipaddr);
+    break ;
+  }
+
+  return 0;
 }
 
-int parse_cmdline(int argc, char **argv)
+#if 1
+static const char* state2String(int state){
+  if (state == 0)
+    return "CLOSED_STATE";
+  if (state == ZOO_CONNECTING_STATE)
+    return "CONNECTING_STATE";
+  if (state == ZOO_ASSOCIATING_STATE)
+    return "ASSOCIATING_STATE";
+  if (state == ZOO_CONNECTED_STATE)
+    return "CONNECTED_STATE";
+  if (state == ZOO_EXPIRED_SESSION_STATE)
+    return "EXPIRED_SESSION_STATE";
+  if (state == ZOO_AUTH_FAILED_STATE)
+    return "AUTH_FAILED_STATE";
+
+  return "INVALID_STATE";
+}
+
+static const char* type2String(int state){
+  if (state == ZOO_CREATED_EVENT)
+    return "CREATED_EVENT";
+  if (state == ZOO_DELETED_EVENT)
+    return "DELETED_EVENT";
+  if (state == ZOO_CHANGED_EVENT)
+    return "CHANGED_EVENT";
+  if (state == ZOO_CHILD_EVENT)
+    return "CHILD_EVENT";
+  if (state == ZOO_SESSION_EVENT)
+    return "SESSION_EVENT";
+  if (state == ZOO_NOTWATCHING_EVENT)
+    return "NOTWATCHING_EVENT";
+
+  return "UNKNOWN_EVENT_TYPE";
+}
+#endif
+
+int get_result_code(void)
 {
-  char ch = 0, *ep = 0;
-  const char *shortopts = "c:p:e:";  
-  const struct option longopts[] = {  
-    {"conf", required_argument, NULL, 'c'},  
-    {"app", required_argument, NULL, 'p'},  
-    {"entry", required_argument, NULL, 'e'},  
-    {0, 0, 0, 0},  
-  };  
+  return __sync_fetch_and_and(&g_zkcInfo.res_code,ZOK);
+}
 
-  while ((ch = getopt_long(argc,argv,shortopts,longopts,NULL))!=-1) {
-    switch(ch) {
-      case 'c':
-        ep = stpncpy(g_appInfo.conf,optarg,PATH_MAX);
-        break ;
+bool is_conf_node(char *data)
+{
+  return data && !strcmp(data,g_zkcInfo.confNode);
+}
 
-      case 'p':
-        ep = stpncpy(g_appInfo.app,optarg,PATH_MAX);
-        break ;
+bool is_master_node(char *data)
+{
+  return data && !strcmp(data,g_zkcInfo.masterNode);
+}
 
-      case 'e':
-        ep = stpncpy(g_appInfo.entry,optarg,PATH_MAX);
-        break ;
+void create_complete(int rc, const char *name, const void *data) 
+{
+  /* completion creation of the znode */
+  __sync_lock_test_and_set(&g_zkcInfo.res_code,rc);
 
-      default:
-        printf("unknown option '%c'\n",ch);
-        return -1 ;
+  sem_post(&g_zkcInfo.s);
+
+  if (data)
+    free((void*)data);
+}
+
+void stat_complete(int rc, const struct Stat *stat, const void *data) 
+{
+  //fprintf(stderr,"rc=%d, data: %s\n",rc,(char*)data);
+
+  if (data) 
+    free((void*)data);
+}
+
+void get_complete(int rc, const char *value, int value_len, 
+   const struct Stat *stat, const void *data) 
+{
+#if 0
+	fprintf(stderr, "%s: rc = %d\n", (char*)data, rc);
+	if (value) {
+			fprintf(stderr, " value_len = %d\n", value_len);
+			assert(write(2, value, value_len) == value_len);
+	}
+	fprintf(stderr, "\nStat:\n");
+#endif
+
+  /* get the 'conf' node ok */
+  if (is_conf_node((char*)data)) {
+    /* TODO: process the config contents */
+    if (rc==ZOK && value_len>0) {
     }
+
+    __sync_lock_test_and_set(&g_zkcInfo.res_code,rc);
+
+    sem_post(&g_zkcInfo.s);
+  }
+
+  if (data)
+    free((void*)data);
+}
+
+
+void watcher(zhandle_t *zzh, int type, int state, const char *path, void* context)
+{
+  fprintf(stderr, "Watcher %s state = %s", type2String(type), state2String(state));
+  
+  /* the master node is deleted */
+  if (is_master_node((char*)path) && type==ZOO_DELETED_EVENT) {
+
+    __sync_lock_test_and_set(&g_zkcInfo.res_code,ZOK);
+
+    sem_post(&g_zkcInfo.s);
+  }
+
+}
+
+int init_zkc_ha(void)
+{
+  if (!*g_zkcInfo.zkhosts) {
+    fprintf(stderr,"no zookeeper host list given!\n");
+    return -1;
+  }
+
+  if (!*g_zkcInfo.app) {
+    fprintf(stderr,"no application file given!\n");
+    return -1;
+  }
+
+  if (!*g_zkcInfo.conf) {
+    fprintf(stderr,"no config file given!\n");
+    return -1;
+  }
+
+  if (access(g_zkcInfo.app,F_OK|X_OK)) {
+    fprintf(stderr,"can't access application file '%s'\n",g_zkcInfo.app);
+    return -1;
+  }
+
+  if (!access(g_zkcInfo.conf,F_OK|R_OK|W_OK)) {
+    fprintf(stderr,"warning: the config file '%s' will be overwirtten!\n",g_zkcInfo.conf);
+  }
+
+  if (sem_init(&g_zkcInfo.s,0,0)) {
+    fprintf(stderr,"sem init fail: %s",strerror(errno));
+    return -1;
+  }
+
+  if (get_master_addr()) {
+    return -1;
+  }
+
+  /* get entry name from application path */
+  {
+    char *p = strrchr(g_zkcInfo.app,'/');
+    char *ep= stpncpy(g_zkcInfo.entry,p?(p+1):g_zkcInfo.app,256);
     *ep= '\0';
   }
+
+  sprintf(g_zkcInfo.confNode,"/%s/conf",g_zkcInfo.entry);
+  sprintf(g_zkcInfo.masterNode,"/%s/master",g_zkcInfo.entry);
+
+  /* init the zookeeper client handle */
+  g_zkcInfo.zh = zookeeper_init(g_zkcInfo.zkhosts, watcher, 30000, 0, 0, 0);
+  if (!g_zkcInfo.zh) {
+    fprintf(stderr,"init zookeeper client failed\n");
+    return -1;
+  }
+
   return 0;
 }
 
-int init(void)
+
+/* the main zookeeper client process */
+int zkc_ha_process(void)
 {
-  bzero(&g_appInfo,sizeof(g_appInfo));
+  int ret = 0;
+  const char *pMastNode = g_zkcInfo.masterNode ;
+  const char *pConfNode = g_zkcInfo.confNode ;
+
+  while (1) {
+
+    /* 
+     * create the $appname/master 
+     */
+    ret = zoo_acreate(g_zkcInfo.zh, pMastNode, g_zkcInfo.ipAddr, 
+      strlen(g_zkcInfo.ipAddr), &ZOO_OPEN_ACL_UNSAFE, ZOO_EPHEMERAL,
+      create_complete, strdup(pMastNode));
+    if (ret) {
+      fprintf(stderr,"error create '%s'\n",pMastNode);
+      return -1;
+    }
+
+    sem_wait(&g_zkcInfo.s);
+
+    if ((ret=get_result_code())==ZOK) {
+      fprintf(stderr,"\ncreate '%s' ok\n",pMastNode);
+      break ;
+    }
+
+    /* node exists, wait for its delete event */
+    if (ret==ZNODEEXISTS) {
+      printf("\nwaiting for create '%s'...\n",pMastNode);
+
+      /* add watch */
+      ret = zoo_aexists(g_zkcInfo.zh, pMastNode, 1, stat_complete, 
+        /*strdup(pMastNode)*/0);
+      if (ret==-1) {
+        fprintf(stderr,"error add watch on '%s'\n", pMastNode);
+        return -1;
+      }
+
+      sem_wait(&g_zkcInfo.s);
+
+      /* the master node is not existing now, 
+       *  try to create it */
+    } 
+    else if (ret==ZNONODE) {
+      fprintf(stderr,"the upper node of '%s' may not "
+        "exists, create it first\n", pMastNode);
+      return -1;
+    }
+
+  } // end while
+
+
+  /* read config */
+  ret = zoo_aget(g_zkcInfo.zh,pConfNode,1,get_complete,
+      strdup(pConfNode));
+  if (ret==-1) {
+    fprintf(stderr,"error get '%s'\n",pConfNode);
+    return -1;
+  }
+
+  sem_wait(&g_zkcInfo.s);
+
+  if (get_result_code()!=ZOK) {
+    fprintf(stderr,"get '%s' fail\n",pConfNode);
+    return -1;
+  }
 
   return 0;
 }
 
-int check_params(void)
+int init_zkc_create(void)
 {
-  if (!*g_appInfo.entry) {
+  if (!*g_zkcInfo.zkhosts) {
+    fprintf(stderr,"no zookeeper host list given!\n");
+    return -1;
+  }
+
+  if (!*g_zkcInfo.entry) {
     fprintf(stderr,"no zookeeper entry given!\n");
-    help();
-  }
-
-  if (!*g_appInfo.app) {
-    fprintf(stderr,"no application file given!\n");
-    help();
-  } else if (access(g_appInfo.app,F_OK|X_OK)) {
-    fprintf(stderr,"can't access application file '%s'\n",g_appInfo.app);
     return -1;
   }
 
-  if (!*g_appInfo.conf) {
+  if (!*g_zkcInfo.conf) {
     fprintf(stderr,"no config file given!\n");
-    help();
-  } else if (!access(g_appInfo.conf,F_OK|R_OK|W_OK)) {
-    fprintf(stderr,"warning: the config file '%s' will be overwirtten!\n",g_appInfo.conf);
-    return 0;
+    return -1;
+  }
+
+  if (access(g_zkcInfo.conf,F_OK|R_OK)) {
+    fprintf(stderr,"the config file '%s' not exists!\n",g_zkcInfo.conf);
+    return -1;
+  }
+
+  if (sem_init(&g_zkcInfo.s,0,0)) {
+    fprintf(stderr,"sem init fail: %s",strerror(errno));
+    return -1;
+  }
+
+  /* init the zookeeper client handle */
+  g_zkcInfo.zh = zookeeper_init(g_zkcInfo.zkhosts, watcher, 30000, 0, 0, 0);
+  if (!g_zkcInfo.zh) {
+    fprintf(stderr,"init zookeeper client failed\n");
+    return -1;
   }
 
   return 0;
 }
 
-
-int main(int argc, char *argv[])
+int zkc_create_znode(void)
 {
-  if (init()) {
+  int ret = 0;
+  char zpath[PATH_MAX] = "";
+  char contents[1<<20] = "";
+  ssize_t szCont = 0L;
+
+  snprintf(zpath,PATH_MAX,"/%s",g_zkcInfo.entry);
+
+  while (1) {
+    /* 
+     * create the /$appname znode
+     */
+    ret = zoo_acreate(g_zkcInfo.zh, zpath, "new", 
+      3, &ZOO_OPEN_ACL_UNSAFE, 0,
+      create_complete, strdup(zpath));
+    if (ret) {
+      fprintf(stderr,"error create '%s'\n",zpath);
+      return -1;
+    }
+
+    sem_wait(&g_zkcInfo.s);
+
+    if ((ret=get_result_code())!=ZOK && ret!=ZNODEEXISTS) {
+      fprintf(stderr,"\ncreate '%s' fail: %d\n",
+        zpath,ret);
+      return -1;
+    }
+
+    if (strstr(zpath,"/conf")) {
+      fprintf(stderr,"done\n");
+      break ;
+    }
+
+    /* 
+     * create the /$appname/conf znode 
+     */
+    strcat(zpath,"/conf");
+  }
+
+  /* read configs */
+  {
+    int fd = open(g_zkcInfo.conf,0,0777);
+    /* the default storage size of a znode is 1M */
+
+    if (fd<0) {
+      fprintf(stderr,"error open config file '%s'\n",g_zkcInfo.conf);
+      return -1;
+    }
+
+    szCont = read(fd,contents,1<<20);
+    close(fd);
+
+    if (szCont==-1) {
+      fprintf(stderr,"error read config file '%s': %s\n",
+        g_zkcInfo.conf,strerror(errno));
+      return -1;
+    }
+  }
+
+  /* write config into /$appname/conf */
+  ret = zoo_aset(g_zkcInfo.zh, zpath, contents, szCont, -1, stat_complete,
+      /*strdup(zpath)*/0);
+  if (ret) {
+    fprintf(stderr,"error write '%s'\n",zpath);
     return -1;
   }
 
-  if (parse_cmdline(argc,argv)) {
+  fprintf(stderr,"refresh config done\n");
+
+  return 0;
+}
+
+int init_zkc_release(void)
+{
+  if (!*g_zkcInfo.zkhosts) {
+    fprintf(stderr,"no zookeeper host list given!\n");
     return -1;
   }
 
-  if (check_params()) {
+  if (!*g_zkcInfo.entry) {
+    fprintf(stderr,"no zookeeper entry given!\n");
     return -1;
   }
+
+  if (sem_init(&g_zkcInfo.s,0,0)) {
+    fprintf(stderr,"sem init fail: %s",strerror(errno));
+    return -1;
+  }
+
+  /* init the zookeeper client handle */
+  g_zkcInfo.zh = zookeeper_init(g_zkcInfo.zkhosts, watcher, 30000, 0, 0, 0);
+  if (!g_zkcInfo.zh) {
+    fprintf(stderr,"init zookeeper client failed\n");
+    return -1;
+  }
+
+  return 0;
+}
+
+void delete_complete(int rc, const void *data) 
+{
+  fprintf(stderr, "%s: rc = %d\n", (char*)data, rc);
+
+  if (data)
+    free((void*)data);
+}
+
+int zkc_release_znode(void)
+{
+  int ret = 0;
+  char zpath[PATH_MAX] = "";
+  
+  /* try to delete the '/conf' node */
+  sprintf(zpath,"/%s/conf",g_zkcInfo.entry);
+  ret = zoo_adelete(g_zkcInfo.zh, zpath, -1, delete_complete, 
+      strdup(zpath));
 
 
 
   return 0;
 }
+
 
 
